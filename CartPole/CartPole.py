681a6a14d8fec719a647bdf858fc9e2a00554adf
hrcarryu <495958672@qq.com>
2022-04-18 15:07:55 +0800

    renmae

--- CartPole/CartPole.py
+++ CartPole/CartPole.py
@@ -1,11 +1,13 @@
 import gym
-import pygame
-import sys
-env = gym.make('CartPole-v1')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-
-# pygame.quit()
-# sys.exit()
+env = gym.make('CartPole-v0')
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done: # 如果结束, 则退出循环
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
